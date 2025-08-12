76477e3c2488d7df528ce2aa08fea612d625c3b9
Vaishnavi Patil <vaishnavianilpatil0@gmail.com>
2025-08-12 10:02:49 +0530

    Final Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BankComponent } from './dashboard/bank/bank.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { DonorRegistrationComponent } from './donor-registration/donor-registration.component';
@@ -10,6 +12,8 @@ import { OrganizationRegistrationComponent } from './organization-registration/o
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },  // Default route
   { path: 'home', component: HomeComponent }, // Home Page
+  { path: 'dashboard', component: DashboardComponent }, 
+  { path: 'bank', component: BankComponent }, 
   { path: 'login', component: LoginComponent },
   { path: 'donor-registration', component: DonorRegistrationComponent },
   { path: 'organization-registration', component: OrganizationRegistrationComponent },
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.css'
+  styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
-
-}
+export class DashboardComponent {}
